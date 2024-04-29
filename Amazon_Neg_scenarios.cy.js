f6cadb78db4e4897b7bc8eb5080bcb0c7d2e4ffd
Tamilarasi-Thenmozhi <160009729+Tamilarasi-Thenmozhi@users.noreply.github.com>
2024-04-29 19:42:46 +0530

    Create Amazon_Neg_scenarios.cy.js

---
+++ Amazon_Neg_scenarios.cy.js
@@ -0,0 +1,29 @@
+import LoginPage from '../support/pages/LoginPage';
+import HomePage from '../support/pages/HomePage';
+import CartPage from '../support/pages/CartPage';
+
+describe('Invalid login and cart functionality', () => {
+    const loginPage = new LoginPage();
+    const homePage = new HomePage();
+    const cartPage = new CartPage();
+
+
+    it('should not login with invalid emailid and password', () => {
+        loginPage.visitLoginPage();
+        loginPage.login('tamilarasimthenmozhi@gmail', 'test');
+        cy.wait(2000)
+    });
+
+    it('should redirect guest user to sign in screen when trying to checkout', () => {
+        homePage.searchForItem('boat earbuds');
+        homePage.verifyItemAddedToCart();
+        cartPage.navigateToCartPage();
+        cy.url().should('include', '/signin');
+    });
+
+    it("should not show 'Proceed to buy' button when cart is empty", () => {
+        cartPage.navigateToCartPage();
+        cy.contains('Your Amazon Cart is empty').should('be.visible');
+        cy.get('#sc-buy-box-ptc-button').should('not.exist');
+    });
+});
